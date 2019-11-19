24fdfbde4a1cc2ab2eee12f8b500b36681cb17c1
Kapitoshka <kapitoshka.maksimov@gmail.com>
2019-11-19 18:37:49 +0500

    'ver40'

--- botbet4.py
+++ botbet4.py
@@ -20,12 +20,21 @@ def send_anytext(message):
                'Current worth is' + ' ' + str(worth)
         bot.send_message(chat_id, text, reply_markup=keyboard())
         worth += 20
+    if message.text == 'Make bet':
+        text = 'How much do you want to bet?'
+        bot.send_message(chat_id, text, reply_markup=force)
+        worth += int(message.text)
+
 
 def keyboard():
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     button1 = types.KeyboardButton('Make min bet')
     markup.add(button1)
     return markup
+
+def force():
+    force_markup = types.ForceReply()
+    tb.send_message(chat_id, 'Choose your bet', reply_markup=force_markup)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True, interval=0, timeout=20)
