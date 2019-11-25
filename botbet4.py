dd6632b017c472e5e581b0f40dcc9c49789a591f
Kapitoshka <kapitoshka.maksimov@gmail.com>
2019-11-25 13:50:09 +0500

    'ver205'

--- botbet4.py
+++ botbet4.py
@@ -53,8 +53,8 @@ def send_anytext(message):
     elif message.text == 'krism end':
         ok = 0
         bot.send_message(chat_id, 'Completed')
-        bot.send_message(win, 'Вы выйграли этот аукцион под номером' + str(ids[:1]) + ', отправьте сумму' + str(worth) +
-                         'на Qiwi кошелёк +79058638358')
+        bot.send_message(win, 'Вы выйграли этот аукцион под номером' + str(lot_info[:1]) + ', отправьте сумму'
+                         + str(worth) +'на Qiwi кошелёк +79058638358')
         worth = 0
     elif message.text == 'Минимальная ставка' and ok == 1:
         worth += 20
