56d50aa9f8ecd8dac9c515cf3117ae682962b797
Kapitoshka <kapitoshka.maksimov@gmail.com>
2019-12-06 22:22:57 +0500

    'new4'

--- botbet3.py
+++ botbet3.py
@@ -1,6 +1,6 @@
 import requests
 import telebot
-import urllib3
+import urllib2
 import re
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
@@ -11,6 +11,14 @@ url = 'http://lyceum-kungur.ru/%d0%b8%d0%b7%d0%bc%d0%b5%d0%bd%d0%b5%d0%bd%d0%b8%
       '%d1%80%d0%b0%d1%81%d0%bf%d0%b8%d1%81%d0%b0%d0%bd%d0%b8%d0%b8/'
 all_links = []
 domain = 'https://drive.google.com/'
+
+
+def find(message):
+    html = urlopen(url)
+    bs = BeautifulSoup(html, 'lxml')
+    links = bs.find('div', {'class': 'BlockContent-body'}).findAll('a')
+    for link in links():
+        bot.send.message(message.chat.id, link)
 
 
 def find_urls(message):
@@ -25,12 +33,11 @@ def find_urls(message):
         bot.send_message(message.chat.id, 'error')
 
 
-
 @bot.message_handler(content_types=['text'])
 def send_anytext(message):
     chat_id = message.chat.id
     if message.text == 'go':
-        find_urls(message)
+        find(message)
         bot.send_message(chat_id, all_links)
 
 
