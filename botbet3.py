f332473030d19e1aa0245e297c4411fd3401c5d5
Kapitoshka <kapitoshka.maksimov@gmail.com>
2019-12-06 22:35:20 +0500

    'new55'

--- botbet3.py
+++ botbet3.py
@@ -11,6 +11,8 @@ url = 'http://lyceum-kungur.ru/%d0%b8%d0%b7%d0%bc%d0%b5%d0%bd%d0%b5%d0%bd%d0%b8%
       '%d1%80%d0%b0%d1%81%d0%bf%d0%b8%d1%81%d0%b0%d0%bd%d0%b8%d0%b8/'
 all_links = []
 domain = 'https://drive.google.com/'
+s = requests.Session()
+
 
 
 def find(message):
@@ -21,24 +23,21 @@ def find(message):
         bot.send.message(message.chat.id, link)
 
 
-def find_urls(message):
+def find_urls():
     global all_links
-    try:
-        res = requests.get(url, timeout=30)
-        soup = BeautifulSoup(res.text, 'lxml')
-        for link in soup.find_all('a', href=True):
-            all_links.add(link['href'])
-            bot.send.message(message.chat.id, '1done')
-    except requests.exceptions.ConnectionError:
-        bot.send_message(message.chat.id, 'error')
+    response = requests.get(url)
+    response.status_code
+    return response.text
 
 
 @bot.message_handler(content_types=['text'])
 def send_anytext(message):
     chat_id = message.chat.id
     if message.text == 'go':
-        find(message)
-        bot.send_message(chat_id, all_links)
+        bs = BeautifulSoup(find_urls(), 'lxml')
+        links = bs.find('div,'{'class' : 'BlockContent-body'}).findAll('a')
+        for link in links:
+            bot.send.message(chat_id, link)
 
 
 if __name__ == '__main__':
