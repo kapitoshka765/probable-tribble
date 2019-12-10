08572ef0c571874d45e0fa281d2b7ded4dc736bf
Kapitoshka <kapitoshka.maksimov@gmail.com>
2019-12-10 23:08:02 +0500

    'veeee2'

--- botbet3.py
+++ botbet3.py
@@ -8,7 +8,8 @@ from requests_html import HTMLSession
 
 token = '790148299:AAFRAXTGp1SoWhQW_FxhOWqBBgxEH43ZAuY'
 bot = telebot.TeleBot(token)
-url = 'https://habr.com/ru/post/280238/'
+url = 'http://lyceum-kungur.ru/%d0%b8%d0%b7%d0%bc%d0%b5%d0%bd%d0%b5%d0%bd%d0%b8%d1%8f-%d0%b2-%d1%80%d0%b0%d1%81%' \
+      'd0%bf%d0%b8%d1%81%d0%b0%d0%bd%d0%b8%d0%b8/'
 all_links = []
 domain = 'https://drive.google.com/'
 s = requests.Session()
@@ -19,7 +20,7 @@ def find(message):
     session = HTMLSession()
     html = session.get(url)
     bs = BeautifulSoup(html.text)
-    link_list = bs.find('div', {'class': 'default-block__content'})
+    link_list = bs.find('div', {'class': 'BlockContent'})
     print(link_list)
     items = link_list.find_all('a')
     for item in items:
