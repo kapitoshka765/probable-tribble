c84058a18cfd18ba767be921586f70f0989db1b0
Kapitoshka <kapitoshka.maksimov@gmail.com>
2019-12-10 21:34:04 +0500

    'ver1'

--- botbet3.py
+++ botbet3.py
@@ -7,8 +7,7 @@ from urllib.parse import urlparse
 
 token = '790148299:AAFRAXTGp1SoWhQW_FxhOWqBBgxEH43ZAuY'
 bot = telebot.TeleBot(token)
-url = 'http://lyceum-kungur.ru/%d0%b8%d0%b7%d0%bc%d0%b5%d0%bd%d0%b5%d0%bd%d0%b8%d1%8f-%d0%b2-' \
-      '%d1%80%d0%b0%d1%81%d0%bf%d0%b8%d1%81%d0%b0%d0%bd%d0%b8%d0%b8/'
+url = 'https://habr.com/ru/post/280238/'
 all_links = []
 domain = 'https://drive.google.com/'
 s = requests.Session()
@@ -20,7 +19,7 @@ def find(message):
     bs = BeautifulSoup(html, 'lxml')
     links = bs.find('div', {'class': 'BlockContent-body'}).findAll('a')
     for link in links():
-        bot.send.message(message.chat.id, link)
+        all_links.append(link)
 
 
 def find_urls():
@@ -34,11 +33,8 @@ def find_urls():
 def send_anytext(message):
     chat_id = message.chat.id
     if message.text == 'go':
-        bs = BeautifulSoup(find_urls(), 'lxml')
-        links = bs.find('div', {'class' : 'BlockContent-body'}).findAll('a')
-        for link in links:
-            bot.send.message(chat_id, link)
-
+        find(message)
+        bot.send.message(chat_id, str(all_links))
 
 if __name__ == '__main__':
     bot.polling(none_stop=True, interval=0, timeout=20)
