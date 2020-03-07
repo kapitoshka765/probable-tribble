d2ebf77d80502f3b3dcb9d43a5cfb8bf0d70e1d2
Kapitoshka <kapitoshka.maksimov@gmail.com>
2020-03-07 15:40:10 +0500

    'zhopa

--- botbet3.py
+++ botbet3.py
@@ -9,86 +9,71 @@ from urllib.parse import urlparse
 from requests_html import HTMLSession
 from telebot import types
 
-token = '790148299:AAFRAXTGp1SoWhQW_FxhOWqBBgxEH43ZAuY'
+token = '705618645:AAHMT7Pn1qTnvnajzvGEeCuLO5fmSTGQa8M'
+nuzno = '603577729'
 bot = telebot.TeleBot(token)
-url = 'http://lyceum-kungur.ru/%d0%b8%d0%b7%d0%bc%d0%b5%d0%bd%d0%b5%d0%bd%d0%b8%d1%8f-%d0%b2-%d1%80%d0%b0%d1%81%' \
-      'd0%bf%d0%b8%d1%81%d0%b0%d0%bd%d0%b8%d0%b8/'
-all_links = []
-all_text = []
-all_ids =[]
-all_days = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'cуббота']
-domain = 'https://drive.google.com/'
-s = requests.Session()
-
-
-def find():
-    global all_links
-    global all_text
-    session = HTMLSession()
-    html = session.get(url)
-    bs = BeautifulSoup(html.text)
-    link_list = bs.find('div', {'class': 'entry'})
-    print(link_list)
-    items = link_list.find_all('a')
-    if len(all_links) > len(all_text) or len(all_text) > len(all_links):
-        all_links = []
-        all_text = []
-    for item in items:
-        link = item.get('href')
-        text = item.text
-        if link not in all_links:
-            all_links.append(link)
-        if text not in all_text:
-            all_text.append(text)
+name1 = ''
+telephon1 = ''
+other1 = ''
+start = []
+telephon = []
+name = []
+other =[]
+need = ['89', '7 9', '+7 9', '8 9', '7 (9', '79']
 
 
 @bot.message_handler(content_types=['text'])
 def send_anytext(message):
+    global name1
+    global telephon1
+    global other1
+    global other
+    global start
+    global telephon
     chat_id = message.chat.id
-    if chat_id not in all_ids:
-        all_ids.append(chat_id)
-    if message.text == 'Расписание':
-        find()
-        bot.send_message(chat_id, 'Выберите нужный день', reply_markup=days())
-    if message.text == 'Другое':
-        find()
-        bot.send_message(chat_id, 'Выберите нужный пункт', reply_markup=another())
-    if message.text in all_text:
-        a = message.text
-        aa = all_text.index(a)
-        bot.send_message(chat_id, str(all_links[aa]), reply_markup=starting())
-    if message.text == 'krism id':
-        bot.send_message(chat_id, len(all_ids), reply_markup=starting())
-    if message.text == 'krism send':
-        for i in all_ids:
-            bot.send_message(i, message.text, reply_markup=starting())
-    elif all_days.count(message.text) and message.text != 'Расписание' and message.text != 'Другое':
-        bot.send_message(chat_id, 'Такой команды не существует, выберите одну, нажав на 4 точки', reply_markup=starting())
+    if message.text == 'Записаться':
+        ubrat()
+        start.append(chat_id)
+        bot.send_message(chat_id, 'Введите своё имя и фамилию')
+    if message.text != 'Записаться' and chat_id in start:
+        name1 = message.text
+        start = []
+        telephon.append(chat_id)
+        bot.send_message(chat_id, 'Введите свой телефон')
+    if message.text != 'Записаться' and chat_id in telephon:
+        telephon1 = message.text
+        telephon = []
+        other.append(chat_id)
+        bot.send_message(chat_id, 'Введите причину обращения')
+    if message.text != 'Записаться' and chat_id in other:
+        other1 = message.text
+        bot.send_message(nuzno, 'Запись от ' + name1 + 'с телефоном ' + telephon1 + 'по причине ' + other1)
+        ubrat()
+        bot.send_message(chat_id, 'Спасибо за обращение, ожидайте звонка')
+    else:
+        bot.send_message(chat_id, 'Нажмите на кнопку', reply_markup=starting())
+
+
+def ubrat():
+    global start1
+    global other1
+    global telephon1
+    global start
+    global other
+    global telephon
+    start = []
+    other = []
+    telephon= []
+    start1 = ''
+    other1 = ''
+    telephon1 = ''
 
 
 def starting():
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True, row_width=1)
-    button = types.KeyboardButton('Расписание')
-    button1 = types.KeyboardButton('Другое')
-    markup.row(button, button1)
+    button = types.KeyboardButton('Записаться')
+    markup.row(button)
     return markup
-
-
-def days():
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True, row_width=1)
-    for opa in range(len(all_text)):
-         if 'февраля (' in str(all_text[opa]) or 'февраля  (' in str(all_text[opa]):
-             markup.row(types.KeyboardButton(str(all_text[opa])))
-    return markup
-
-
-def another():
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True, row_width=1)
-    for opa in range(len(all_text)):
-        if 'февраля (' not in str(all_text[opa]) and 'февраля  (' not in str(all_text[opa]):
-                markup.row(types.KeyboardButton(str(all_text[opa])))
-    return markup
-
 
 
 if __name__ == '__main__':
